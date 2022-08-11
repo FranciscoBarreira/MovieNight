6174791cf0a57dce6b97c73e7f2094d4505f6771
FranciscoBarreira <Francisco.barreira1992@gmail.com>
2022-08-11 15:06:59 +0000

    add fixtures and registered apps in admin

--- products/admin.py
+++ products/admin.py
@@ -1,3 +1,8 @@
 from django.contrib import admin
-
+from .models import Movie, Category, Edition, Sub_Category
 # Register your models here.
+
+admin.site.register(Movie)
+admin.site.register(Category)
+admin.site.register(Sub_Category)
+admin.site.register(Edition)
