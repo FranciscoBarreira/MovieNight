e867f950172f933f43d14b692cc99256d65ecaec
FranciscoBarreira <Francisco.barreira1992@gmail.com>
2022-08-14 14:37:11 +0000

    add category sorting to main-nav

--- products/views.py
+++ products/views.py
@@ -1,5 +1,7 @@
-from django.shortcuts import render, get_object_or_404
-from .models import Movie
+from django.shortcuts import render, redirect, reverse, get_object_or_404
+from django.contrib import messages
+from django.db.models import Q
+from .models import Movie, Category
 
 # Create your views here.
 
@@ -7,9 +9,29 @@ def all_products(request):
     """ A view to show all products, including sorting and search queries """
 
     products = Movie.objects.all()
+    query = None
+    categories = None
+
+    if request.GET:
+        if 'movie_category' in request.GET:
+            categories = request.GET['movie_category'].split(',')
+            products = products.filter(movie_category__movie_category__in=categories)
+            categories = Category.objects.filter(movie_category__in=categories)
+
+    if request.GET:
+        if 'q' in request.GET:
+            query = request.GET['q']
+            if not query:
+                messages.error(request, "Please enter a search term")
+                return redirect(reverse('products'))
+            
+            queries = Q(title__icontains=query) | Q(synopsis__icontains=query)
+            products = products.filter(queries)
 
     context = {
         'products': products,
+        'search_term': query,
+        'current_categories': categories,
     }
 
     return render(request, 'products/products.html', context)
