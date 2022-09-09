85866f41ef1137cff0551d659f6aa606ad9c0ecb
FranciscoBarreira <Francisco.barreira1992@gmail.com>
2022-09-09 15:05:13 +0000

    changed products.html

--- checkout/webhook_handler.py
+++ checkout/webhook_handler.py
@@ -70,8 +70,9 @@ class StripeWH_Handler:
                 profile.default_country = shipping_details.address.country
                 profile.default_postcode = shipping_details.address.postal_code
                 profile.default_town_or_city = shipping_details.address.city
-                profile.default_street_address1 = shipping_details.address.line1
-                profile.default_street_address2 = shipping_details.address.line2
+                shipping_address = shipping_details.address
+                profile.default_street_address1 = shipping_address.line1
+                profile.default_street_address2 = shipping_address.line2
                 profile.default_county = shipping_details.address.state
                 profile.save()
 
